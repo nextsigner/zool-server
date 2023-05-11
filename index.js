3724b77fa58d1c62d66b96b2941fb8812f4ec022
nextsigner <nextsigner@gmail.com>
2023-05-11 18:48:04 -0300

    Se edita zooldata.js

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ var mongoose = require('mongoose');
 require('./zoolusers')(app);
 require('./zoolparams')(app);
 require('./zoolappids')(app);
+require('./zooldata')(app);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
---
+++ zooldata.js
@@ -0,0 +1,51 @@
+﻿module.exports=function(app){
+
+    getZoolData = function(req, res){
+        console.log('getZoolData... ');
+        console.log('Dia: '+req.query.d);
+        //res.redirect('/res-add-producto.html?res=no'+mensajes.length)
+        //Registra el ZoolUser porque no existe ninguno con ese nombre
+        let jsonRes={isData:false}
+        const exec = require('child_process').exec;
+        exec('python3 "/home/ns/nsp/zool-release/py/astrologica_swe_v2.py" '+req.query.d+' '+req.query.m+' '+req.query.a+' '+req.query.h+' '+req.query.min+' '+req.query.gmt+' '+req.query.lat+' '+req.query.lon+' T /home/ns/nsp/zool-release', (err, stdout, stderr) => {
+                 if (err) {
+                     console.error(err);
+                     jsonRes={isData:false, isError:true, error: err}
+                     res.status(200).send(jsonRes);
+                     return;
+                 }
+                 jsonRes={isData:true, isError:false, data: JSON.parse(stdout)}
+                 //jsonRes=JSON.parse(stdout);
+                 //console.log(JSON.stringify(jsonRes, null, 2));
+                 res.status(200).send(jsonRes);
+             });
+        //res.status(200).send(jsonRes)
+        return
+    }
+    getUZoolandVersion = function(req, res){
+        console.log('getUZoolandVersion... ');
+        let jsonRes={isData:false}
+        const exec = require('child_process').exec;
+        exec('ls /home/ns/nsp/zool-server/files/*.zip', (err, stdout, stderr) => {
+                 if (err) {
+                     console.error(err);
+                     jsonRes={isData:false, isError:true, error: err}
+                     res.status(200).send(jsonRes);
+                     return;
+                 }
+                 let v=0;
+                 let m0=stdout.split('\n')
+                 let m1=(''+m0[0]).split('/')
+                 let zipFileName=m1[m1.length-1]
+                 jsonRes={isData:true, isError:false, data: zipFileName}
+                 //jsonRes=JSON.parse(stdout);
+                 //console.log(JSON.stringify(jsonRes, null, 2));
+                 res.status(200).send(jsonRes);
+             });
+        //res.status(200).send(jsonRes)
+        return
+    }
+    app.get('/zool/getZoolData', getZoolData);
+    app.get('/zool/getUZoolandVersion', getUZoolandVersion);
+}
+
