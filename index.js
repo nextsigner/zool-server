df4ceabb87ccd1b5e428e69b66fcfa21330e5a6e
nextsigner <nextsigner@gmail.com>
2024-05-07 15:08:07 -0300

    Se agranda fuente

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+//Instrucciones
+
+//Para iniciar servidor en el dominio. Por ejemplo zool.ar:80
+//npm start
+
+//Para probar de un modo local/desarrollo en ip 127.0.0.1:8100
+//npm start localhost
+
+//Para probar de un modo red local/desarrollo en ip diferente a 127.0.0.1:8100
+//npm start localhost host=192.168.1.40
+
+
 var appServerName='zool-server'
 var args = process.argv.slice(2);
-var puertoApp = '8100'; //Puerto que recibe acciones
+var puertoApp = '80'; //Puerto que recibe acciones
 var puertoStatico = '8101'; //Puerto para descargar archivos
 var folderFiles = 'files/';
 var serverEmail=process.env.EMAIL
@@ -23,6 +35,11 @@ for(var i=0;i<argumentos.length;i++){
         localhost=m0[1]
         console.log('Ip: '+localhost)
     }
+    if(arg.indexOf('port=')>=0){
+        let m0=arg.split('port=')
+        puertoApp=m0[1]
+        console.log('Puerto: '+puertoApp)
+    }
 }
 
 
@@ -37,9 +54,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(express.static(__dirname));
-app.set('port', process.env.PORT || puertoApp);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+if(local){
+    puertoApp='8100'
+}
+app.set('port', process.env.PORT || puertoApp);
 
 var mongoose = require('mongoose');
 require('./zoolusers')(app);
@@ -61,7 +81,9 @@ mongoose.connect(urlMongoDatabase, { useNewUrlParser: true }, function (err, res
     }
     console.log(`Conectado a ${urlMongoDatabase}`)
     let host='zool.ar'
-    if(local)host=localhost
+    if(local){
+        host=localhost
+    }
     app.listen(app.get('port'), host, function() {
         console.log('Servidor '+appServerName+' iniciado en '+host+':'+app.get('port'));
         console.log('Puertos: App=' + app.get('port') + '  Files='+ puertoStatico);
